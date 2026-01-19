8497136d6116ea55637f38a6a4ada5e31098d8b6
Alexandra Arias <aleariangar@gmail.com>
2026-01-19 15:19:53 -0500

    feat: set up initial API Gateway with TCP and HTTP communication

---
+++ backend/api-gateway/src/products/products.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Post()
+  create(@Body() dto: CreateProductDto) {
+    return this.productsService.create(dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
